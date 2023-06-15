a609b85fbd6437defb19002260a667ca821e01bf
Amy <wlgpek90@naver.com>
2023-06-15 12:05:00 +0900

    user post success

--- src/main/java/com/playdata/todos/config/JdbcConnection.java
+++ src/main/java/com/playdata/todos/config/JdbcConnection.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class jdbcConnection {
+public class JdbcConnection {
 
-    private final String url = "jdbc:mysql://localhost:3306/test2" +
+    private final String url = "jdbc:mysql://localhost:3306/todos" +
             "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
     private final String root = "root";
     private final String password = "1q2w3e4r!!";
@@ -14,12 +14,14 @@ public class jdbcConnection {
     public Connection getJdbc() {
         Connection conn;
         try {
-//            Class.forName("com.mysql")
+            Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(url, root, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
 //        System.out.println("연동 성공");
 
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
         return conn;
